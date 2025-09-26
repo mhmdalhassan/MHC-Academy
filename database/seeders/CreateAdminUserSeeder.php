8ba0d98dc583fbe97b0d4d7f32f7bc4a637a8b86
Hassan Awad <hassanawadk123@gmail.com>
2025-09-26 14:27:53 +0300

    super admin updates

--- database/seeders/CreateAdminUserSeeder.php
+++ database/seeders/CreateAdminUserSeeder.php
@@ -2,31 +2,33 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
 
 class CreateAdminUserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        $user = User::create([
-            'name' => 'Hardik Savani',
-            'email' => 'admin@gmail.com',
-            'password' => bcrypt('123456')
-        ]);
+        // Create Super Admin user
+        $user = User::firstOrCreate(
+            ['email' => 'hassanawadk123@gmail.com'],
+            [
+                'name' => 'Super Admin',
+                'password' => Hash::make('hassan242001'),
+            ]
+        );
 
-        $role = Role::create(['name' => 'Admin']);
+        // Create Admin role
+        $role = Role::firstOrCreate(['name' => 'Admin']);
 
+        // Give Admin role all permissions
         $permissions = Permission::pluck('id', 'id')->all();
-
         $role->syncPermissions($permissions);
 
+        // Assign Admin role to user
         $user->assignRole([$role->id]);
     }
 }
